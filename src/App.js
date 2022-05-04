3b6c8292d2755ec448d49f81c113667195bd3413
vihang <vihangk64@gmail.com>
2022-05-04 23:13:48 +0530

    removed files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, { useState } from 'react';
 
 function App() {
+  let [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+      <h1>{count}</h1>
+      <div>
+          <button className="" onClick={(e)=> setCount(count-5) }>-5</button>
+          <button className="m-l-10" onClick={(e)=> setCount(count-1) }>-</button>
+          <button className="m-l-10" onClick={(e)=> setCount(0) }>Reset</button>
+          <button className="m-l-10" onClick={(e)=> setCount(count+1) }>+</button>
+          <button className="m-l-10" onClick={(e)=> setCount(count+5) }>+5</button>
+      </div>
     </div>
   );
 }
